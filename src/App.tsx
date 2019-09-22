2151dea9428ca137e34fc087fae45b32a91bb394
Orlondo <o.rue@sbcglobal.net>
2019-09-22 15:30:52 -0400

    Added: Spinning Cube / 100% width and height of window on resize

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,52 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import * as THREE from 'three';
+
+const Three: React.FC = () => {
+  const scene = new THREE.Scene();
+  const camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
+  const renderer = new THREE.WebGLRenderer();
+
+  renderer.setSize( window.innerWidth, window.innerHeight );
+  document.body.appendChild( renderer.domElement );
+
+  var geometry = new THREE.BoxGeometry( 1, 1, 1 );
+  var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+  var cube = new THREE.Mesh( geometry, material );
+
+  scene.add( cube );
+  camera.position.z = 5;
+
+  const animate = function () {
+    requestAnimationFrame( animate );
+    cube.rotation.x += 0.01;
+    cube.rotation.y += 0.01;
+    renderer.render( scene, camera );
+  };
+
+  window.addEventListener('resize', () => {
+    renderer.setSize( window.innerWidth, window.innerHeight );
+    camera.aspect = window.innerWidth / window.innerHeight;
+
+    camera.updateProjectionMatrix();
+  })
+
+  animate();
+
+  // useEffect(() => {
+  //   console.log('Mounted');
+  //   animate();
+  // }, [])
+  
+  
+
+  return <></>
+}
 
 const App: React.FC = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Three  />
     </div>
   );
 }
